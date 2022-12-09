564e550161899e14e9cb8e1ec14e2c8d2fcef780
Jakub <96346556+Khazaxa@users.noreply.github.com>
2022-12-09 22:25:19 +0100

    create notes v1

---
+++ Lab. 02. Zmienne i typy danych, warunkowy wybór/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string LastName = Console.ReadLine();
+        int Age = Convert.ToInt32(Console.ReadLine());
+        int AgePension = Convert.ToInt32(Console.ReadLine());
+        int x = AgePension - Age;
+
+        
+        Console.WriteLine($"Witaj, {LastName}!");
+
+        if(Age<0 || AgePension<0)
+        {
+            Console.WriteLine("Wiek nie może być ujemny!");
+        }
+        else if(Age < AgePension)
+        {
+            Console.WriteLine($"Do emerytury brakuje Ci {x} lat!");
+        }
+        else
+        {
+            Console.WriteLine("Jesteś emerytem!");
+        }
+    }
+}
